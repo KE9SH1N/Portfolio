3e0251dd3fe0d3aebf0ecb4c0f13cb9ff3ba5531
unknown <K3NSHI9@gmail.com>
2021-08-08 14:18:44 +0600

    adding plugins at vendor adding styles and client section at index files

--- js/main.js
+++ js/main.js
@@ -15,4 +15,38 @@ $(document).ready(function() {
     })
 
     $('.button-group #btn1').trigger(click);
+
 });
+
+
+$(document).ready(function() {
+    $('.test-popup-link').magnificPopup({
+        type:'image',
+        gallery:{enabled:true}
+    });
+  });
+
+
+  //owl-carousel
+
+  $(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        
+        autoplay:true,
+        dots:true,
+        items:2,
+        responsive:{
+            0:{
+                item:1
+            },
+            544:{
+                item:2
+            }
+        }
+
+    });
+  });
+
+
+
